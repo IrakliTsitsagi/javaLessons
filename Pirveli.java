1a624c9b9f43fa2dff1c91f457c6bf614b77c8ad
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-09-28 22:04:23 +0400

    added first file

---
+++ Pirveli.java
@@ -0,0 +1,16 @@
+public class Pirveli {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+        System.out.println("Hello world!!");
+        System.out.println("Hello world!!");
+        System.out.println("Hello world!!");
+        System.out.println("Hello world!!");
+        System.out.println("Hello world!!");
+
+        // komentari
+
+        int a =3;
+        int b =5;
+        System.out.println(a+b);
+    }
+}
