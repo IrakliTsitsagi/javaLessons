9bb12a71ab1ec127b777a2539a12f2bfa52ddc6d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-09-29 19:42:53 +0400

    Add files via upload

---
+++ javasawyisi.java
@@ -0,0 +1,21 @@
+public class javasawyisi {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        int z = 10;
+        if (x > y) {
+            if (x > z)
+                System.out.println(x);
+         else
+                System.out.println(z);
+
+            } else {
+                if (y > z)
+                    System.out.println(y);
+
+                else
+                    System.out.println(z);
+
+            }
+        }
+    }
