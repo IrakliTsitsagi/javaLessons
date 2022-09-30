4553d2285ba89b0e3c6cf1f4ba0e04dd2be9adc5
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-09-30 11:52:54 +0400

    Update saati.java

--- saati.java
+++ saati.java
@@ -1,12 +1,13 @@
-public class saati {
+public class Main {
     public static void main(String[] args) {
         int wamebi = 3665;
         int wamiSaatshi = 3600;
         int wutiSaatshi = 60;
-        int saati= 3665 / wamiSaatshi;
-        int wuti = 65 / wutiSaatshi;
+        int saati= wamebi / wamiSaatshi;
+        int wutebi = wamebi % wamiSaatshi;
+        int wuti = wutebi / wutiSaatshi;
 
-        int wami = 65 % 60;
+        int wami = wutebi % 60;
         System.out.println(saati + ":" +  wuti + ":" + wami);
     }
 }
