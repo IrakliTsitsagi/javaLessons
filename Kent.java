94a3fab93ee30224901535fb0479cc0c95bbad8d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-10-04 12:22:27 +0400

    Add files via upload

---
+++ Kent.java
@@ -0,0 +1,14 @@
+public class Kent {
+    public static void main(String[] args){
+        for (int x = 99; x > 1; x--) {
+            if (x == 7) {
+                continue;
+            }
+            if (x % 2 !=0){
+                System.out.println(x);
+            }
+
+        }
+    }
+}
+
