0fb58324ca169fa5f0a85bb37b102e698319ee7d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-10-04 12:15:19 +0400

    Add files via upload

---
+++ Luw.java
@@ -0,0 +1,10 @@
+public class Luw {
+    public static void main(String[] args){
+        for (int x = 0; x < 51; x++) {
+            if (x % 2 ==0){
+                System.out.println(x);
+            }
+
+        }
+    }
+}
