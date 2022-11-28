83a34f6de1ac7ff3bee796dc156b149ce005426b
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-11-28 13:32:35 +0400

    Add files via upload

---
+++ Gacvla.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Gacvla {
+    public static void main(String[] args) {
+        //varianti1
+    /*    int a = 1;
+        int b = 2;
+        System.out.println(a +","+ b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a +","+ b); */
+
+        //varianti 2
+          /*    int c;
+        int a = 1;
+        int b = 2;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a +","+ b);   */
+
+
+        //asoebi
+
+    //    String a = "gori";
+     //   char[] masivi = a.toCharArray();
+     //   int i,k,t;
+     //  for(i=0; i<k/2; i++){
+      //     t=a[i];
+        //   a[i] = a[k-i-1];
+         //  a[k-i-1]= t;
+       }
+       // System.out.println(masivi);
+
+       //ricxbebi
+      /*  int[] masivi = {1,2,3,4};
+            System.out.println(masivi);
+        for(int i=masivi.length-1; i >= 0; i--)
+            System.out.print(masivi[i]);*/
+
+
+        }
+       // System.out.println(a.charAt(i);
+
+
